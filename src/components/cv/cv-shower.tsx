d25f0834e820113a0c9233cfbe28ea6dc9d8025a
Hayot Olivier <olivier.hayot.dev@gmail.com>
2024-09-22 20:14:51 +0200

    updated cv

--- src/components/cv/cv-shower.tsx
+++ src/components/cv/cv-shower.tsx
@@ -1,14 +1,17 @@
-import { Box, Button } from "@chakra-ui/react";
-import { PDFDownloadLink } from "@react-pdf/renderer";
-import CvPdf from "./cv-pdf";
+import { Box, Button, Link } from "@chakra-ui/react";
+//import { PDFDownloadLink } from "@react-pdf/renderer";
+//import CvPdf from "./cv-pdf";
+import CV from "../../../public/olivier_hayot_cv_septembre_2024.pdf" 
 
 export default function CvShower(){
     return(
         <Box>
             <Button>
+                <Link href={CV} target="_blank" download>Download my CV now!</Link>
+                {/*
                 <PDFDownloadLink document={<CvPdf />} fileName="olivier_hayot_cv.pdf">
                     {({loading}) => (loading ? 'Loading document...' : 'Download my CV now!')}
-                </PDFDownloadLink>
+                </PDFDownloadLink>*/}
             </Button>
         </Box>
     );
